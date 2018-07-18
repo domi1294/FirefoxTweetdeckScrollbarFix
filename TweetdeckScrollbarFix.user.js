c81bb999dae2c2cbfed4a68099034d50fb474fb1
Dominik Beppler <github@dbeppler.de>
2018-07-18 09:45:40 +0200

    Add files via upload

---
+++ TweetdeckScrollbarFix.user.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name Tweetdeck Scrollbar Fix for Firefox
+// @namespace dbeppler.de
+// @match *://tweetdeck.twitter.com*
+// @require https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js
+// @require http://rawgit.com/gromo/jquery.scrollbar/master/jquery.scrollbar.min.js
+// @version 1.0.0
+// @run-at document-end
+// ==/UserScript==
+(function(){
+  console.log("Scrollbar Fix initialized");
+  //var link = window.document.createElement("link");
+  //link.rel = "stylesheet";
+  //link.type = "text/css";
+  //link.href = "https://rawgit.com/gromo/jquery.scrollbar/master/jquery.scrollbar.css";
+  //$("HEAD").append(link);
+  //jQuery(".scrollbar-inner").scrollbar();
+})();
